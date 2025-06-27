2ff7cbc99cea470979163a830d6e64deb50c20c0
MohamedElsafty <me5574871@gmail.com>
2025-06-27 17:41:05 +0300

    create PageViewOfferCard

--- lib/features/home/presentation/pages/widgets/custom_offer_card.dart
+++ lib/features/home/presentation/pages/widgets/custom_offer_card.dart
@@ -12,63 +12,67 @@ class CustomOfferCard extends StatelessWidget {
     final double h = MediaQuery.of(context).size.height;
     final double w = MediaQuery.of(context).size.width;
 
-    return Container(
-      height: h * 0.24,
-      width: w * 0.8,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: AssetImage(AssetsData.offerCard),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        height: h * 0.35,
+        width: w * 0.5,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(AssetsData.offerCard),
+          ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "50-40%",
-              style: Styles.textStyle24bold.copyWith(color: Colors.white),
-            ),
-            Text(
-              "OFF",
-              style: Styles.textStyle24bold.copyWith(color: Colors.white),
-            ),
-            Text(
-              "Now in",
-              style: Styles.textStyle14.copyWith(color: Colors.white),
-            ),
-            Text(
-              "(product) All colours",
-              style: Styles.textStyle14.copyWith(color: Colors.white),
-            ),
-            SizedBox(height: 12,),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                side: const BorderSide(color: Colors.white),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "50-40%",
+                style: Styles.textStyle24bold.copyWith(color: Colors.white),
+              ),
+              Text(
+                "OFF",
+                style: Styles.textStyle24bold.copyWith(color: Colors.white),
+              ),
+              Text(
+                "Now in",
+                style: Styles.textStyle14.copyWith(color: Colors.white),
+              ),
+              Text(
+                "(product) All colours",
+                style: Styles.textStyle14.copyWith(color: Colors.white),
+              ),
+              SizedBox(height: 12,),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  side: const BorderSide(color: Colors.white),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                 ),
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Text(
+                      "Shop Now ",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    Icon(
+                      Icons.arrow_forward_rounded,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
               ),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Text(
-                    "Shop Now ",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  Icon(
-                    Icons.arrow_forward_rounded,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-            ),
 
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/features/home/presentation/pages/widgets/home_view_body.dart
+++ lib/features/home/presentation/pages/widgets/home_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
-import 'custom_offer_card.dart';
+import 'list_view_offer_card.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(children: [CustomAppBar(), CustomOfferCard()]),
+        child: Column(children: [CustomAppBar(), PageViewOfferCard()]),
       ),
     );
   }
