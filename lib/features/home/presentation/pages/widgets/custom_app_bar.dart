e02fa2e6b19a4ca736a8c61507c2c1bffd290c75
MohamedElsafty <me5574871@gmail.com>
2025-06-27 16:34:31 +0300

    create homeview

---
+++ lib/features/home/presentation/pages/widgets/custom_app_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/assets.dart';
+import '../../../../../core/styles.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double  h =MediaQuery.of(context).size.height;
+    final double  w =MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          GestureDetector(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),         child: Container(
+              height: h*0.05,
+              width: w*0.11,
+              color: Color(0xffF2F2F2),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Image.asset(
+                  "assets/icons/Vector.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            ),
+          ),
+          Row(
+            children: [
+              Text("E", style: Styles.textStyle30bold,),
+              Text("mptoria", style: Styles.textStyle24bold.copyWith(
+                  color: Color(0xff008CFF)
+              ),),
+            ],
+          ),
+          Image.asset(AssetsData.logo)
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/pages/widgets/home_view_body.dart
+++ lib/features/home/presentation/pages/widgets/home_view_body.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'custom_app_bar.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+
+      body:
+        SafeArea(
+          child: Column(
+            children: [
+              CustomAppBar(),
+            ],
+          ),
+        )
+    );
   }
 }
+
