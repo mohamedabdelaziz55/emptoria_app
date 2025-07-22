dea2e3e55d20d6853166f0aee0ccd74b2cd583dc
MohamedElsafty <me5574871@gmail.com>
2025-07-22 18:23:43 +0300

    finish sing up

--- lib/features/auth/presentation/pages/sign_up_screen.dart
+++ lib/features/auth/presentation/pages/sign_up_screen.dart
@@ -1,22 +1,16 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/routes/app_route.gr.dart';
 import '../../../../core/utils/app_colors.dart';
+import '../river_pod/sign_up_river_pod/sing_up_river_pod.dart';
 import '../widgets/login_button.dart';
 import '../widgets/signup_form_fields.dart';
 import '../widgets/signup_header.dart';
 
 @RoutePage()
-class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({super.key});
-
-  @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
-}
-
-class _SignUpScreenState extends State<SignUpScreen> {
-  bool _obscurePassword = true;
-  bool _obscureConfirmPassword = true;
+class SignUpScreen extends ConsumerWidget {
+  SignUpScreen({super.key});
 
   final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
@@ -25,11 +19,15 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final _confirmPasswordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final media = MediaQuery.of(context);
     final isPortrait = media.orientation == Orientation.portrait;
     final screenWidth = isPortrait ? media.size.width : media.size.height;
     final screenHeight = isPortrait ? media.size.height : media.size.width;
+
+    final obscurePassword = ref.watch(obscurePasswordProvider);
+    final obscureConfirmPassword = ref.watch(obscureConfirmPasswordProvider);
+    final isLoading = ref.watch(isSignUpLoadingProvider);
 
     return Scaffold(
       body: Form(
@@ -37,10 +35,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         child: SafeArea(
           child: Stack(
             children: [
-              SignUpHeader(
-                screenWidth: screenWidth,
-                screenHeight: screenHeight,
-              ),
+              SignUpHeader(screenWidth: screenWidth, screenHeight: screenHeight),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
                 child: SingleChildScrollView(
@@ -53,10 +48,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           SizedBox(height: screenHeight * 0.27),
                           const Text(
                             'Create Account',
-                            style: TextStyle(
-                              fontSize: 28,
-                              fontWeight: FontWeight.bold,
-                            ),
+                            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                           ),
                           SizedBox(height: screenHeight * 0.01),
                           Row(
@@ -89,26 +81,35 @@ class _SignUpScreenState extends State<SignUpScreen> {
                             nameController: _nameController,
                             emailController: _emailController,
                             passwordController: _passwordController,
-                            confirmPasswordController:
-                                _confirmPasswordController,
-                            obscurePassword: _obscurePassword,
-                            obscureConfirmPassword: _obscureConfirmPassword,
-                            onTogglePassword: () => setState(
-                              () => _obscurePassword = !_obscurePassword,
-                            ),
-                            onToggleConfirmPassword: () => setState(
-                              () => _obscureConfirmPassword =
-                                  !_obscureConfirmPassword,
-                            ),
+                            confirmPasswordController: _confirmPasswordController,
+                            obscurePassword: obscurePassword,
+                            obscureConfirmPassword: obscureConfirmPassword,
+                            onTogglePassword: () => ref
+                                .read(obscurePasswordProvider.notifier)
+                                .state = !obscurePassword,
+                            onToggleConfirmPassword: () => ref
+                                .read(obscureConfirmPasswordProvider.notifier)
+                                .state = !obscureConfirmPassword,
                           ),
                           Align(
                             alignment: Alignment.center,
-                            child: LoginButton(
+                            child: isLoading
+                                ? CircularProgressIndicator(color: AppColors.black)
+                                : LoginButton(
                               image: 'assets/icons/arrow.png',
                               width: screenWidth * 0.32,
                               height: screenHeight * 0.065,
                               text: "Sign Up",
-                              onTap: () => context.pushRoute(DashboardRoute()),
+                              onTap: () {
+                                ref.read(signUpProvider).signUp(
+                                  name: _nameController.text,
+                                  email: _emailController.text,
+                                  password: _passwordController.text,
+                                  confirmPassword: _confirmPasswordController.text,
+                                  formKey: _formKey,
+                                  context: context,
+                                );
+                              },
                             ),
                           ),
                           SizedBox(height: screenHeight * 0.03),
